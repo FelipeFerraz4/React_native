35c1fde4598875042646951069dec369f19bb128
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-09-30 21:17:09 -0300

    finalização do projeto

--- ConsultaFakeApi/src/paginas/Repositorios/index.js
+++ ConsultaFakeApi/src/paginas/Repositorios/index.js
@@ -1,9 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, FlatList, TouchableOpacity} from 'react-native';
 import estilos from './estilos';
+import {pegarRepositoriosDoUsuario} from '../../servicos/requisicoes/repositorios';
+import {useIsFocused} from '@react-navigation/native';
 
 export default function Repositorios({route, navigation}) {
   const [repo, setRepo] = useState([]);
+  const estaNaTela = useIsFocused();
+
+  useEffect(() => {
+    async function pegarRepositorio() {
+      const resultado = await pegarRepositoriosDoUsuario(route.params.id);
+      setRepo(resultado);
+    }
+    pegarRepositorio();
+  }, [estaNaTela, route.params.id]);
 
   return (
     <View style={estilos.container}>
@@ -12,9 +23,28 @@ export default function Repositorios({route, navigation}) {
       </Text>
       <TouchableOpacity
         style={estilos.botao}
-        onPress={() => navigation.navigate('CriarRepositorio')}>
+        onPress={() =>
+          navigation.navigate('CriarRepositorio', {id: route.params.id})
+        }>
         <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
       </TouchableOpacity>
+
+      <FlatList
+        data={repo}
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{width: '100%'}}
+        keyExtractor={repokey => repo.id}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={estilos.repositorio}
+            onPress={() => navigation.navigate('InfoRepositorio', {item})}>
+            <Text style={estilos.repositorioNome}>{item.name}</Text>
+            <Text style={estilos.repositorioData}>
+              Atualizado em {item.data}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
--- ConsultaFakeApi/src/servicos/api.js
+++ ConsultaFakeApi/src/servicos/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.209.220:3000/',
+  baseURL: 'http://192.168.0.173:3000/',
 });
 
 export default api;
